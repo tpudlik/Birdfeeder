ade3597ac3bd9ff76d69392ff6efdf498670daa4
Ted Pudlik <tpudlik@gmail.com>
2014-04-05 16:33:30 -0400

    Fixes to webcam.py

--- webcam.py
+++ webcam.py
@@ -4,12 +4,12 @@
 import subprocess
 import datetime
 
-def shot = take_snapshot():
+def take_snapshot():
     """ Take a (succession of?) snapshots and save them to disk. Return
         filename of one of the shapshots.
         
     """
     filename = datetime.datetime.now().strftime("%Y-%M-%d-%H-%M-%S")
     filename = filename + '.jpg'
-    subprocess.call('fswebcam -r 640x480 -d /dev/video0 ' + filename)
+    subprocess.call(['fswebcam', '-r', '640x480', '-d', '/dev/video0', 'filename'])
     return filename
