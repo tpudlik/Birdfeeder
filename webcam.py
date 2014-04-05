d96ead10b9b7d619218a20ac655586e74a83ff46
Ted Pudlik <tpudlik@gmail.com>
2014-04-05 16:49:38 -0400

    Correction to webcam

--- webcam.py
+++ webcam.py
@@ -3,13 +3,15 @@
 
 import subprocess
 import datetime
+import os
 
 def take_snapshot():
     """ Take a (succession of?) snapshots and save them to disk. Return
         filename of one of the shapshots.
         
     """
-    filename = datetime.datetime.now().strftime("%Y-%M-%d-%H-%M-%S")
+    filename = datetime.datetime.now().strftime("%Y-%m-%d-%H-%M-%S")
     filename = filename + '.jpg'
-    subprocess.call(['fswebcam', '-r', '640x480', '-d', '/dev/video0', 'filename'])
+    subprocess.Popen(['fswebcam', '-r', '640x480', '-d', '/dev/video0', 'filename'],
+                     cwd=os.getcwd())
     return filename
