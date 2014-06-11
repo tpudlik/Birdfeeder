f9f260d45d05ce26d5684eb91ee2cff75b7add5a
Ted Pudlik <tpudlik@gmail.com>
2014-06-11 19:57:46 -0400

    Minor changes:  * Updated README.md  * Added more parameters to parameters.py, organized them better  * Made main.py an executable script

--- README.md
+++ README.md
@@ -4,6 +4,7 @@ Paparazzi Birdfeeder
 The paparazzi birdfeeder detects the presence of a bird, takes its picture
 and posts the picture to a dedicated Twitter feed.
 
-The hardware consists of a webcam and passive IR sensor connected to a 
-Raspberry Pi camera.  The entire setup can be powered for hours from a power
- pack.
+The hardware consists of a Raspberry Pi camera module, a passive IR sensor
+and a sonic ranger connected to a Raspberry Pi.  The entire setup can be
+powered for hours from a power pack, but is designed for tethered operation.
+
--- parameters.py
+++ parameters.py
@@ -1,13 +1,34 @@
 # Parameters used by main.py
 
-# How many seconds between queries of the PIR?
-DETECTOR_DELAY = 1
+
+# GPIO setup
+# ==========
 
 # Which GPIO pin (in the BCM scheme) is the PIR detector on?
 PIR_PIN = 18
 
+# Which GPIO pin is the sonic ranger trigger on?
+TRIG = 23
+
+# Which GPIO pin is the sonic ranger echo on?
+ECHO = 24
+
+
+# Delays
+# ======
+
+# How many seconds between queries of the PIR?
+DETECTOR_DELAY = 1
+
+# For how many seconds should the sonic ranger be allowed to settle?
+SETTLETIME = 0.3
+
 # How many seconds between pictures?
 PHOTO_DELAY = 5
 
-# Should the photos be tweeted?
+
+# Twitter integration
+# ===================
+
+# Should the photos be tweeted (True or False)?
 TWEET = False
