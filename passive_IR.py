f088482a69b894404f26279876bd9c33e26f3106
Ted Pudlik <tpudlik@gmail.com>
2014-06-24 22:31:29 -0400

    PIR interface now uses the GPIO.wait_for_edge method.  This cleans up the code considerably and (if the RPi.GPIO docs are to be believed) saves CPU time.

--- passive_IR.py
+++ passive_IR.py
@@ -20,25 +20,18 @@ class PIR():
             once every detector_delay seconds.
         """
         self.pin = pir_pin
-        self.previous_pir = 0
-        # The use of previous_pir prevents a single detection being
-        # reported multiple times.
         self.detector_delay = detector_delay
         logging.info('Passive IR detector initialized')
     
     def listen(self):
-        " Loop until a detection event is recorded, then return True."
+        " Wait for a detection event, then return True."
         GPIO.setup(self.pin, GPIO.IN)
-        while True:
-            time.sleep(self.detector_delay)
-            self.current_pir = GPIO.input(self.pin)
-            if self.previous_pir == 0 and self.current_pir == 1:
-                GPIO.cleanup()
-                logging.info('Passive IR detected bird')
-                return True
-            self.previous_pir = self.current_pir
+        time.sleep(self.detector_delay)
+        GPIO.wait_for_edge(self.pin, GPIO.RISING)
+        logging.info('Passive IR detected bird')
+        return True
 
 
 if __name__ == '__main__':
     p = PIR()
-    while True: p.listen()
+    while True: print p.listen()
