120e6b66be9a7a2006604a23f036ee5a31d0ac68
Ted Pudlik <tpudlik@gmail.com>
2014-07-02 22:15:36 -0400

    Fixed bug in twitter.py

--- twitter.py
+++ twitter.py
@@ -2,7 +2,7 @@
 
 import logging
 from twython import Twython, TwythonError
-from access_tokes import T_APP_KEY, T_APP_SECRET, T_OAUTH_TOKEN, T_OAUTH_SECRET
+from access_tokens import T_APP_KEY, T_APP_SECRET, T_OAUTH_TOKEN, T_OAUTH_TOKEN_SECRET
 
 def get_auth():
     """ Get OAUTH tokens.  (This function isn't actually used.)"""
