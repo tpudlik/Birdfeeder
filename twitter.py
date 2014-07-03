8b08e73dcf370625b6efa8d1b633e219b01a546c
Ted Pudlik <tpudlik@gmail.com>
2014-07-02 21:50:00 -0400

    Corrected newly-introduced bug in twitter.py

--- twitter.py
+++ twitter.py
@@ -2,11 +2,11 @@
 
 import logging
 from twython import Twython, TwythonError
-from access_tokes import D_APP_KEY, D_APP_SECRET, D_OAUTH_TOKEN, D_OAUTH_SECRET
+from access_tokes import T_APP_KEY, T_APP_SECRET, T_OAUTH_TOKEN, T_OAUTH_SECRET
 
 def get_auth():
     """ Get OAUTH tokens.  (This function isn't actually used.)"""
-    twitter = Twython(D_APP_KEY, D_APP_SECRET)
+    twitter = Twython(T_APP_KEY, T_APP_SECRET)
     auth = twitter.get_authentication_tokens()
     return auth
 
@@ -14,8 +14,8 @@ def update_image(shot, status='Bird spotted!'):
     """ Upload the image in the file 'shot' to Twitter account. """
     # Requires Authentication as of Twitter API v1.1
     logger = logging.getLogger('main.twitter')
-    twitter = Twython(D_APP_KEY, D_APP_SECRET,
-                      D_OAUTH_TOKEN, D_OAUTH_TOKEN_SECRET)
+    twitter = Twython(T_APP_KEY, T_APP_SECRET,
+                      T_OAUTH_TOKEN, T_OAUTH_TOKEN_SECRET)
     photo = open(shot, 'rb')
     try:
         twitter.update_status_with_media(media=photo, status=status)
