c83aeed1a44d6c9ccac7fb98103c77f4798f1976
Ted Pudlik <tpudlik@gmail.com>
2014-07-02 21:48:44 -0400

    The twitter.py module now imports the access tokens from a separate file.

--- twitter.py
+++ twitter.py
@@ -2,15 +2,11 @@
 
 import logging
 from twython import Twython, TwythonError
-
-APP_KEY = 'ySpw09Es52O9gbsgJM68HBP2F'
-APP_SECRET = 'lhZRj9R418U1W0OmA6NEJfhcHfQ2md4eRTBrjJvjjfxql9IRAC'
-OAUTH_TOKEN = '2429040776-0rkZJGKOpG5jdLe9UG1oxpRCx8x3IcWUadR8m8e'
-OAUTH_TOKEN_SECRET = 'yEfYZUd9M9JWtWHlx4XbDXdUmClH1sGCMpwsBNXtcpUwY'
+from access_tokes import D_APP_KEY, D_APP_SECRET, D_OAUTH_TOKEN, D_OAUTH_SECRET
 
 def get_auth():
     """ Get OAUTH tokens.  (This function isn't actually used.)"""
-    twitter = Twython(APP_KEY, APP_SECRET)
+    twitter = Twython(D_APP_KEY, D_APP_SECRET)
     auth = twitter.get_authentication_tokens()
     return auth
 
@@ -18,7 +14,8 @@ def update_image(shot, status='Bird spotted!'):
     """ Upload the image in the file 'shot' to Twitter account. """
     # Requires Authentication as of Twitter API v1.1
     logger = logging.getLogger('main.twitter')
-    twitter = Twython(APP_KEY, APP_SECRET, OAUTH_TOKEN, OAUTH_TOKEN_SECRET)
+    twitter = Twython(D_APP_KEY, D_APP_SECRET,
+                      D_OAUTH_TOKEN, D_OAUTH_TOKEN_SECRET)
     photo = open(shot, 'rb')
     try:
         twitter.update_status_with_media(media=photo, status=status)
