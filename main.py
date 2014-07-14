bf7553a19b65b6ececa4a1ce9dec9a4e41f1b9ec
Ted Pudlik <tpudlik@gmail.com>
2014-07-13 21:38:11 -0400

    The twitter and dbox modules are now only imported if they will be used.  This implies that a user who is not interested in a particular functionality doesn't need to install the relevant SDK to run the program.

--- main.py
+++ main.py
@@ -9,14 +9,16 @@ a bird out there.  Tweet the picture afterwards!
 import time
 import datetime
 import logging
-import twitter
 import picamera
 import RPi.GPIO as GPIO
 from passive_IR import PIR
 from usonic import Ranger
-import dbox
 from parameters import * # I don't like this approach, I'd like to validate
                          # the parameters.  How to do this better?
+if TWEET:
+    import twitter
+if DBOX:
+    import dbox
 
 # Configure loggers
 # ============================================================================
